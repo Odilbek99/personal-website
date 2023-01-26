f4aaa5224626f51374a11c58c82873b5b1fe967e
Odilbek99 <umaralievodili@gmail.com>
2023-01-26 21:38:16 +0300

    Skills Section

--- website/views.py
+++ website/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from .models import NavBarModel, HomePageModel, AboutModel, StatsModel
+from .models import NavBarModel, HomePageModel, AboutModel, StatsModel, SkillsModel
 # Create your views here.
 def index(request):
     navbar = NavBarModel.objects.all()
     home = HomePageModel.objects.all()
     about = AboutModel.objects.get(id=1)
     stats = StatsModel.objects.all()
+    skills = SkillsModel.objects.all()
+
     context = {
         'navbar': navbar,
         'home': home,
         'about': about,
         'stats': stats,
-    }
+        'skills': skills,
+        'first_skills': skills[:len(skills)//2],
+        'last_skills': skills[len(skills)//2:]
+     }
     return render(request, 'index.html', context=context)
 
 def portfolio_details(request):
